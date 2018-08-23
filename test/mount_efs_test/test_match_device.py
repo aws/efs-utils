07423ed922106597c8d832accaba214d161f10f9
Pit Kleyersburg <pitkley@googlemail.com>
2018-08-23 13:45:59 +0200

    Incorporate PR review by maxbecke
    This includes the following changes:
    
    * Reduce inline comments that were mostly redundant to the code it
    described
    
    * Added another example to the man-page to make clear that TLS is
    supported for CNAMEs. For that matter, all other options will also work
    without change, given that internally the CNAME will only be resolved to
    an FS-ID before handing back over to the same logic that is already in
    place.
    
    * Small modifications to better adhere to the given codestyle.
    
    * Adjusted log messages as suggested.

--- test/mount_efs_test/test_match_device.py
+++ test/mount_efs_test/test_match_device.py
@@ -17,12 +17,6 @@ CORRECT_DEVICE_DESCRIPTORS_FS_ID = [
     ('fs-deadbeef:/some/subpath', ('fs-deadbeef', '/some/subpath')),
     ('fs-deadbeef:/some/subpath/with:colons', ('fs-deadbeef', '/some/subpath/with:colons')),
 ]
-CORRECT_DEVICE_DESCRIPTORS_EFS_FQDN = [
-    ('fs-deadbeef.efs.us-east-1.amazonaws.com', ('fs-deadbeef', '/')),
-    ('fs-deadbeef.efs.us-east-1.amazonaws.com:/', ('fs-deadbeef', '/')),
-    ('fs-deadbeef.efs.us-east-1.amazonaws.com:/some/subpath', ('fs-deadbeef', '/some/subpath')),
-    ('fs-deadbeef.efs.us-east-1.amazonaws.com:/some/subpath/with:colons', ('fs-deadbeef', '/some/subpath/with:colons')),
-]
 CORRECT_DEVICE_DESCRIPTORS_CNAME_DNS = [
     ('custom-cname.example.com', ('fs-deadbeef', '/')),
     ('custom-cname.example.com:/', ('fs-deadbeef', '/')),
@@ -34,13 +28,6 @@ CORRECT_DEVICE_DESCRIPTORS_CNAME_DNS = [
 def test_match_device_correct_descriptors_fs_id(mocker):
     for device, (fs_id, path) in CORRECT_DEVICE_DESCRIPTORS_FS_ID:
         assert (fs_id, path) == mount_efs.match_device(None, device)
-
-
-def test_match_device_correct_descriptors_efs_fqdn(mocker):
-    get_dns_name_mock = mocker.patch('mount_efs.get_dns_name', return_value='fs-deadbeef.efs.us-east-1.amazonaws.com')
-    for device, (fs_id, path) in CORRECT_DEVICE_DESCRIPTORS_EFS_FQDN:
-        assert (fs_id, path) == mount_efs.match_device(None, device)
-    get_dns_name_mock.assert_called()
 
 
 def test_match_device_correct_descriptors_cname_dns_primary(mocker):
@@ -87,18 +74,6 @@ def test_match_device_correct_descriptors_cname_dns_amongst_invalid(mocker):
     gethostbyname_ex_mock.assert_called()
 
 
-def test_match_device_wrong_efs_dns_name(mocker, capsys):
-    get_dns_name_mock = mocker.patch('mount_efs.get_dns_name', return_value='fs-deadbeef.efs.us-west-1.amazonaws.com')
-    with pytest.raises(SystemExit) as ex:
-        mount_efs.match_device(None, 'fs-deadbeef.efs.us-east-1.amazonaws.com:/')
-
-    assert 0 != ex.value.code
-    out, err = capsys.readouterr()
-    assert 'Fully qualified EFS domain name specified' in err
-    assert 'didn\'t match the expected value' in err
-    get_dns_name_mock.assert_called()
-
-
 def test_match_device_unresolvable_domain(mocker, capsys):
     mocker.patch('socket.gethostbyname_ex', side_effect=socket.gaierror)
     with pytest.raises(SystemExit) as ex:
@@ -117,7 +92,7 @@ def test_match_device_no_hostnames(mocker, capsys):
 
     assert 0 != ex.value.code
     out, err = capsys.readouterr()
-    assert 'returned no entries' in err
+    assert 'did not resolve to an EFS mount target' in err
     gethostbyname_ex_mock.assert_called()
 
 
@@ -129,7 +104,7 @@ def test_match_device_no_hostnames2(mocker, capsys):
 
     assert 0 != ex.value.code
     out, err = capsys.readouterr()
-    assert 'returned no entries' in err
+    assert 'did not resolve to an EFS mount target' in err
     gethostbyname_ex_mock.assert_called()
 
 
@@ -141,7 +116,7 @@ def test_match_device_resolve_to_invalid_efs_dns_name(mocker, capsys):
 
     assert 0 != ex.value.code
     out, err = capsys.readouterr()
-    assert 'resolved to no valid/expected EFS DNS name' in err
+    assert 'did not resolve to a valid DNS name' in err
     gethostbyname_ex_mock.assert_called()
 
 
@@ -154,6 +129,6 @@ def test_match_device_resolve_to_unexpected_efs_dns_name(mocker, capsys):
 
     assert 0 != ex.value.code
     out, err = capsys.readouterr()
-    assert 'resolved to no valid/expected EFS DNS name' in err
+    assert 'did not resolve to a valid DNS name' in err
     get_dns_name_mock.assert_called()
     gethostbyname_ex_mock.assert_called()
