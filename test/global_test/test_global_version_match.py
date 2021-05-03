fc626ebafa3b769ead752ba635c47b06fc87c630
Nico Kadel-Garcia <nkadel@gmail.com>
2021-05-03 01:25:40 -0400

    Set SPEC_FILE to amazon-efs-utils.spec.in in tests

--- test/global_test/test_global_version_match.py
+++ test/global_test/test_global_version_match.py
@@ -14,7 +14,7 @@ except ImportError:
     from configparser import ConfigParser
 
 
-SPEC_FILE = 'amazon-efs-utils.spec'
+SPEC_FILE = 'amazon-efs-utils.spec.in'
 DEB_FILE = 'build-deb.sh'
 FILE_LIST = ['build-deb.sh', 'src/watchdog/__init__.py', 'src/mount_efs/__init__.py',
                   'dist/amazon-efs-utils.control', 'build-deb.sh', 'amazon-efs-utils.spec.in']
